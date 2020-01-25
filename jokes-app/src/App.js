a6f3e689601df77fe5dbad3b13fb14d47f039eb9
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-25 13:43:32 -0600

    Added a jokes-app folder for building out the react app.

---
+++ jokes-app/src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => {
+
+  //need a login component.
+  //need a registration component.
+
+    //logged in? Display jokes!
+
+  //logout - needs to be handled client-side so we can clear the token. Server can't do that.
+
+}
+
+export default App;
---
+++ jokes-app/src/components/login.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+const login = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
