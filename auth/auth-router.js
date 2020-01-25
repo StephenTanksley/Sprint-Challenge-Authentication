98b9443cc8955a050feb474e5aadcf3fdfa1f096
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-25 11:26:59 -0600

    Added dependencies. Wrote an auth model file.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET || 'You are the perfect drug.'
+const bcrypt = require('bcryptjs')
+// const authModel = require('')
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res, next) => {
+  try {
+    let { username, password } = req.body
+    const newUser = username && password
+    ? await 
+
+  }
+  catch (error) {
+    next(error)
+  }
+
 });
 
 router.post('/login', (req, res) => {
