16ab9280e51082a8f10a248651c0cfdb99339a45
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-25 11:46:55 -0600

    Configured session to get a JWT set up. Password will be hashed and saved to the user object when a user registers.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,24 +1,60 @@
 const router = require('express').Router();
+const express = require('express')
+
+const authModel = require('./authModel')
+const authorization = require('./authenticate-middleware')
+
 const jwt = require('jsonwebtoken')
-const jwtSecret = process.env.JWT_SECRET || 'You are the perfect drug.'
+const secrets = require('../config/secrets')
 const bcrypt = require('bcryptjs')
-// const authModel = require('')
+
 
 router.post('/register', async (req, res, next) => {
   try {
-    let { username, password } = req.body
-    const newUser = username && password
-    ? await 
+    const saved = await userModel.add(req.body)
+    res.status(201).json(saved)
+  }
+  catch (error) {
+    next(error)
+  }
+});
+
+router.post('/login', (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await userModel.getBy({username}).first()
+    const passwordValid = await bcrypt.compare(password, user.password)
+  
+    if(user && passwordValid) {
+      const token = generateToken(user)
+  
+      res.status(200).json({
+        message: `Welcome, ${user.username}`,
+        token: token,
+      })
+    } else {
+      res.status(401).json({
+        message: 'Invalid credentials',
+      })
+    }
 
   }
   catch (error) {
     next(error)
   }
-
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+const generateToken = (user) => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secrets.jwt, options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
 
 module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+    const decoded = jwt.verify(token, secrets.jwt)
+
+    req.userId = decoded.subject
+    next()
+  }
+  catch (error) {
+    return res.status(401).json({
+      message: 'Invalid credentials.'
+    })
+  }
   res.status(401).json({ you: 'shall not pass!' });
 };
